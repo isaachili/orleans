c2404dec2497423bb4f217770a7463be32fe2662
Isaac Hili <18428880+isaachili@users.noreply.github.com>
2022-07-13 09:21:48 +0000

    Refactor synchronizer to remove redundant code

--- src/Orleans.Runtime/Synchronizer.cs
+++ src/Orleans.Runtime/Synchronizer.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 
 namespace Orleans.Runtime
 {
@@ -54,53 +53,5 @@ namespace Orleans.Runtime
         public States State { get; internal set; }
 
         public IDisposable GrainTimer { get; set; }
-    }
-
-    public class Synchronizer<TGrain, TKey> : Synchronizer
-        where TGrain : Grain, IGrain
-        where TKey : IComparable<TKey>, IComparable, IEquatable<TKey>
-    {
-        // Backing fields
-        private TKey key;
-
-        public Synchronizer(TKey key)
-        {
-            this.Key = key;
-        }
-
-        public Type GrainType => typeof(TGrain);
-
-        public TKey Key
-        {
-            get => this.key;
-            private set => this.key = value switch
-            {
-                string or long or Guid => value,
-                _ => throw new InvalidOperationException("The provided type is not supported by 'key'."),
-            };
-        }
-
-        internal bool VerifyActivationData(IActivationData activationData)
-        {
-            static TKey ChangeType<TValue>(TValue value)
-            {
-                return (TKey)Convert.ChangeType(value, typeof(TKey));
-            }
-
-            if (activationData is not ActivationData ad || ad.GrainType != typeof(TGrain))
-            {
-                return false;
-            }
-
-            var key = this.Key switch
-            {
-                string => ChangeType(activationData.GrainId.PrimaryKeyString),
-                long => ChangeType(activationData.GrainId.PrimaryKeyLong),
-                Guid => ChangeType(activationData.GrainId.PrimaryKey),
-                _ => throw new InvalidOperationException("The provided type is not supported by 'key'."),
-            };
-
-            return key.Equals(this.Key);
-        }
     }
 }
